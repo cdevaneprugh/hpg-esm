3f654fc9a913837e8da2a1e7cb552405870e6477
Cooper DeVane-Prugh <cdevaneprugh@login11.ufhpc>
2025-02-19 09:34:34 -0500

    Added .gitignore and buildpio script, updated configs and README.

---
+++ config/hpg/gnu_hipergator.cmake
@@ -0,0 +1,16 @@
+string(APPEND FFLAGS " -fallow-argument-mismatch -fallow-invalid-boz")
+
+if(DEFINED ENV{PIO})
+  set(PIO_LIBDIR "$ENV{PIO}/lib")
+  set(PIO_INCDIR "$ENV{PIO}/include")
+endif()
+
+set(ESMF_LIBDIR "/apps/gcc/12.2.0/openmpi/4.1.6/esmf/8.7.0/lib/libO/Linux.gfortran.64.openmpi.default")
+set(LAPACK_LIBDIR "/apps/gcc/12.2.0/lapack/3.11.0/lib")
+set(NETCDF_PATH "/apps/gcc/12.2.0/openmpi/4.1.6/netcdf-c/4.9.2")
+set(NETCDF_C_PATH "/apps/gcc/12.2.0/openmpi/4.1.6/netcdf-c/4.9.2")
+set(NETCDF_FORTRAN_PATH "/apps/gcc/12.2.0/openmpi/4.1.6/netcdf-f/4.6.1")
+
+execute_process(COMMAND ${NETCDF_FORTRAN_PATH}/bin/nf-config --flibs OUTPUT_VARIABLE SHELL_CMD_OUTPUT_BUILD_INTERNAL_IGNORE0 OUTPUT_STRIP_TRAILING_WHITESPACE)
+string(APPEND SLIBS " ${SHELL_CMD_OUTPUT_BUILD_INTERNAL_IGNORE0}")
+string(APPEND SLIBS " -L$(LAPACK_LIBDIR) -llapack -lblas")
